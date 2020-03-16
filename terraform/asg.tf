cf3082a04e528477cf8cb099af0366067d4e06fe
Nicholas Law <nick@nicholaslaw.com.au>
2020-03-17 05:43:26 +1100

    Add ASG metrics and scale policies

--- terraform/asg.tf
+++ terraform/asg.tf
@@ -1,7 +1,15 @@
+# ==========================================
+# == KEY PAIR
+# ==========================================
+
 resource "aws_key_pair" "app_kp" {
   key_name   = "${var.repository}-key"
   public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC6pkLMdc205B8kBKxN+UBjoH/ezXqOaWkK1OnkRXvROulrK9X3ENEwW6ApYQ41mRPxyLahOHmfVDe3l7thRUdHBXA1SGjPBVjwmAEB5ABoO6EQNB4KL3N0LKNlAjCAbcw6RgXWjTliHyKWWJZTmjWVE8eO+2VuJZCKDgeKi31eowQEVDWgsCpGsFluC9Gl1IZMtpP44r7UM7X5BiFWDMx7BzUBeHYKaible6jXmeNr8+uGfWdHqFwBC+ylK5brL9CYb97vS4cdAKdyGB+n037zZIjuP/Jc0E0eevRpiouoTGADSjglA/jeW8UOLcNcRwc4UtJQLueSrb19MSOde74Sf8kLIyPs6ZKgvV9Yz2lZL74IiETDdyzOrgAaEjG5OuSFDbdELlgsXJzChmO0zVti5B76UUGlwLZvyF9y/eiowDYNubo1geK3bA9Y5Pqn30Ds5YwmBgWjZRuQTzYPfWuUf+bkSBv71TKMdWZUjqIj8leS5PVNObxRS5RyL8i5g65ND7iKqMZ4bgkH8j7e/yW09n89VBOOIgPz/mo7qA/UiBeXdihP4wgqceZkKxQqkMmXH88L4EKhf2QZrs3bXkYdQpTlO01RWHF3oDD42DuI4e0uQB5lbUvrJRDWli/FggUQ3HZ6ge7aTo+cjjHu4uHQC76AAowohlakk1fbOPRlNw== ${var.repository}"
 }
+
+# ==========================================
+# == LAUNCH CONFIGURATION
+# ==========================================
 
 resource "aws_launch_configuration" "app_lg" {
   name                 = "${var.repository}-lg"
@@ -18,15 +26,26 @@ resource "aws_launch_configuration" "app_lg" {
   }
 }
 
+# ==========================================
+# == ASG
+# ==========================================
+
 resource "aws_autoscaling_group" "asg" {
   name                      = "${var.repository}-asg"
   desired_capacity          = 2
   min_size                  = 2
-  max_size                  = 4
+  max_size                  = 3
   termination_policies      = ["OldestInstance"]
   launch_configuration      = aws_launch_configuration.app_lg.name
   wait_for_elb_capacity     = 1
   wait_for_capacity_timeout = "5m"
+
+  enabled_metrics = [
+    "GroupDesiredCapacity", "GroupInServiceCapacity", "GroupPendingCapacity",
+    "GroupMinSize", "GroupMaxSize", "GroupInServiceInstances", "GroupPendingInstances",
+    "GroupStandbyInstances", "GroupStandbyCapacity", "GroupTerminatingCapacity",
+    "GroupTerminatingInstances", "GroupTotalCapacity", "GroupTotalInstances"
+  ]
 
   vpc_zone_identifier = [
     data.aws_subnet.secure_subnet_1.id,
@@ -59,4 +78,63 @@ resource "aws_autoscaling_group" "asg" {
       propagate_at_launch = false
     },
   ]
+}
+
+# ==========================================
+# == ASG SCALE UP POLICY
+# ==========================================
+
+resource "aws_autoscaling_policy" "scale_up_policy" {
+  name                   = "${var.repository}-scale-up-policy"
+  scaling_adjustment     = "1"
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 150
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_up_alarm" {
+  alarm_name          = "${var.repository}-scale-up-alarm"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "1"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.scale_up_policy.arn]
+}
+
+
+# ==========================================
+# == ASG SCALE DOWN POLICY
+# ==========================================
+
+resource "aws_autoscaling_policy" "scale_down_policy" {
+  name                   = "${var.repository}-scale-down-policy"
+  scaling_adjustment     = "-1"
+  adjustment_type        = "ChangeInCapacity"
+  cooldown               = 150
+  autoscaling_group_name = aws_autoscaling_group.asg.name
+}
+
+resource "aws_cloudwatch_metric_alarm" "scale_down_alarm" {
+  alarm_name          = "${var.repository}-scale-down-alarm"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "0.7"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg.name
+  }
+
+  alarm_actions = [aws_autoscaling_policy.scale_down_policy.arn]
 }
