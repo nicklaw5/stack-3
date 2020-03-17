74fe99c77367b6973055dc7e6b6d826eb9247034
Nicholas Law <nick@nicholaslaw.com.au>
2020-03-18 05:21:19 +1100

    Have user_data script check DB connection

--- terraform/templates/user_data.sh
+++ terraform/templates/user_data.sh
@@ -2,8 +2,20 @@
 
 WEB_ROOT=~/web-root
 
-echo "==> Installing python..."
-apt-get install -y python
+function psql_alias() { PGPASSWORD=postgres psql -h db.demo.nicholaslaw.com.au -p 5432 -U postgres "$@"; }
+
+echo "==> Adding postgres apt repository..."
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | apt-key add -
+sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+apt-get update -y
+
+echo "==> Installing python and psql..."
+apt-get install -y python postgresql-client-11
+echo "==> psql installed: $(psql --version)"
+
+echo "==> Checking DB connection..."
+DB_CONNECTION=$(psql_alias -tAc "SELECT 1")
+echo "DB_CONNECTION: $${DB_CONNECTION}"
 
 echo "==> Creating root web directory..."
 mkdir -p $${WEB_ROOT}
@@ -20,5 +32,5 @@ echo "    <p>Hello :)</p>"      >> index.html
 echo "</body>"                  >> index.html
 echo "</html>"                  >> index.html
 
-echo "==> Starting web server..."
+echo "==> Starting web server in the background..."
 sudo python -m SimpleHTTPServer 80 &
