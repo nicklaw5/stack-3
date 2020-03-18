02e43cfd5974b043e67df50aa128ccef806d0611
Nicholas Law <nick@nicholaslaw.com.au>
2020-03-19 06:09:32 +1100

    Other minor tweaks

--- curl-loop.sh
+++ curl-loop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-while true; do sleep 0.1; curl --silent http://demo.nicholaslaw.com.au/ > /dev/null; done
+while true; do sleep 0.1; curl --silent https://demo.nicholaslaw.com.au/ > /dev/null; done
